1a96e0f0e77c0c8fbe45772019b94718278eeb12
Kipkemoi Fred <fred.kipkemoi@tracom.co.ke>
2024-10-03 17:05:20 +0300

    breaking: doesnt show videos because it is not receiving answer and active statuses, it also has an issue when the same terminal starts a session

--- lib/webrtc_phoenix/sessionmanager.ex
+++ lib/webrtc_phoenix/sessionmanager.ex
@@ -1,143 +1,339 @@
+# defmodule SessionManager do
+#   use GenServer
+#   require Logger
+
+#   @topic "webrtc_session"
+
+#   @enforce_keys [:state]
+#   defstruct clients: %{}, state: :impossible
+
+#   # Starting the GenServer
+#   def start_link(_opts) do
+#     GenServer.start_link(__MODULE__, %SessionManager{state: :impossible}, name: __MODULE__)
+#   end
+
+#   def add_client(user_id, socket) do
+#     Logger.info("add_client called")
+#     GenServer.call(__MODULE__, {:add_client, user_id, socket})
+#   end
+
+#   def remove_client(user_id) do
+#     GenServer.cast(__MODULE__, {:remove_client, user_id})
+#   end
+
+#   def get_clients do
+#     GenServer.call(__MODULE__, :get_clients)
+#   end
+
+#   # Server Callbacks
+
+#   def init(state) do
+#     {:ok, state}
+#   end
+
+#   def handle_call({:add_client, user_id, socket}, _from, state) do
+#     new_state = Map.put(state, user_id, socket)
+#     {:reply, :ok, new_state}
+#   end
+
+#   def handle_call(:get_clients, _from, state) do
+#     {:reply, state, state}
+#   end
+
+#   def handle_cast({:remove_client, user_id}, state) do
+#     new_state = Map.delete(state, user_id)
+#     {:noreply, new_state}
+#   end
+
+#   def reset_clients do
+#     GenServer.cast(__MODULE__, :reset_clients)
+#   end
+
+#   def handle_cast(:reset_clients, _state) do
+#     {:noreply, %{}}
+#   end
+
+
+
+#   def init(state) do
+#     {:ok, state}
+#   end
+
+
+
+#   # Public API
+#   @spec on_session_started(any(), any()) :: any()
+#   def on_session_started(session_id, pid) do
+#     Logger.info("on_session_started called for #{inspect(session_id)}")
+#     GenServer.call(__MODULE__, {:start_session, session_id, pid})
+#   end
+
+#   def on_message(session_id, message) do
+#     GenServer.cast(__MODULE__, {:message, session_id, message})
+#   end
+
+#   def on_session_close(session_id) do
+#     GenServer.call(__MODULE__, {:close_session, session_id})
+#   end
+
+#   def handle_call({:start_session, session_id, pid}, _from, %SessionManager{clients: clients} = state) do
+#     Logger.info("handle_call for session: #{inspect(session_id)}")
+#     Logger.info("handle_call for pid: #{inspect(pid)}")
+
+#     if map_size(clients) >= 2 do
+#         send(pid, :close)
+#         {:reply, :too_many_clients, state}
+#     else
+#         Logger.info("Adding client #{inspect(session_id)}")
+#         new_clients = Map.put(clients, session_id, pid)
+
+#         new_state = if map_size(new_clients) >= 2, do: :ready, else: :impossible
+
+#         # Notify about state update
+#         notify_about_state_update(%{state | clients: new_clients, state: new_state})
+
+#         {:reply, {:ok, session_id}, %{state | clients: new_clients}}
+#     end
+# end
+
+
+#   def handle_call({:close_session, session_id}, _from, %SessionManager{clients: clients} = state) do
+#     Logger.info("Closing session: #{inspect(session_id)}")
+#     new_clients = Map.delete(clients, session_id)
+#     new_state = if map_size(new_clients) < 2, do: :impossible, else: state.state
+#     notify_about_state_update(%{state | clients: new_clients, state: new_state})
+#     {:reply, :ok, %{state | clients: new_clients}}
+#   end
+
+#   def handle_cast({:message, session_id, message}, %SessionManager{clients: clients, state: state} = current_state) do
+#     Logger.info("Received message from #{inspect(session_id)}: #{inspect(message)}")
+
+#     cond do
+#       String.starts_with?(message, "STATE") -> handle_state(session_id, clients[session_id], state)
+#       String.starts_with?(message, "OFFER") -> handle_offer(session_id, message, current_state)
+#       String.starts_with?(message, "ANSWER") -> handle_answer(session_id, message, current_state)
+#       String.starts_with?(message, "ICE") -> handle_ice(session_id, message, current_state)
+#     end
+
+#     {:noreply, current_state}
+#   end
+
+#   # Internal Logic
+#   defp handle_state(session_id, client_pid, state) do
+#     Logger.info("Handling state request for #{inspect(session_id)}")
+#     if Process.alive?(client_pid) do
+#       send(client_pid, {:state, state})
+#     else
+#       Logger.error("Client #{inspect(session_id)} is not alive.")
+#     end
+#   end
+
+#   defp handle_offer(session_id, message, %SessionManager{clients: clients, state: :ready} = state) do
+#     Logger.info("Handling offer from #{inspect(session_id)}")
+#     target_client = find_target_client(session_id, clients)
+#     if target_client do
+#       send(target_client, {:offer, message})
+#     else
+#       Logger.warn("No target client available to send the offer.")
+#     end
+#   end
+
+#   defp handle_answer(session_id, message, %SessionManager{clients: clients, state: :creating} = state) do
+#     Logger.info("Handling answer from #{inspect(session_id)}")
+#     target_client = find_target_client(session_id, clients)
+#     if target_client do
+#       send(target_client, {:answer, message})
+#     else
+#       Logger.warn("No target client available to send the answer.")
+#     end
+#   end
+
+#   defp handle_ice(session_id, message, %SessionManager{clients: clients} = state) do
+#     Logger.info("Handling ICE candidate from #{inspect(session_id)}")
+#     target_client = find_target_client(session_id, clients)
+#     if target_client do
+#       send(target_client, {:ice, message})
+#     else
+#       Logger.warn("No target client available to send the ICE candidate.")
+#     end
+#   end
+
+#   # Assuming this is part of the client process
+# def handle_info({:state, state}, state_data) do
+#   Logger.info("Client received state: #{inspect(state)}")
+#   # Handle the state as needed
+#   {:noreply, state_data}
+# end
+
+#   defp find_target_client(session_id, clients) do
+#     clients
+#     |> Enum.filter(fn {id, _} -> id != session_id end)
+#     |> Enum.map(&elem(&1, 1))
+#     |> List.first()
+#   end
+
+#   defp notify_about_state_update(%SessionManager{clients: clients, state: state}) do
+#     Logger.info("Clients: #{inspect(clients)}")
+#     Logger.info("State: #{inspect(state)}")
+
+#     Enum.each(clients, fn {key, pid} ->
+#         if Process.alive?(pid) do
+#             Logger.info("Before Sent state #{inspect(state)} to client #{inspect(key)} pid #{inspect(pid)}")
+#           send(pid, {:state, state})
+
+#             Logger.info("AfterSent state #{inspect(state)} to client #{inspect(key)} pid #{inspect(pid)}")
+#         else
+#             Logger.error("Client #{inspect(key)} has a dead process with PID: #{inspect(pid)}")
+#         end
+#     end)
+# end
+
+
+# end
+
+
 defmodule SessionManager do
   use GenServer
   require Logger
 
-  @topic "webrtc_session"
+  @session_states [:impossible, :ready, :creating, :active]
 
-  @enforce_keys [:state]
-  defstruct clients: %{}, state: :impossible
-
-  # Starting the GenServer
-  def start_link(_opts) do
-    GenServer.start_link(__MODULE__, %SessionManager{state: :impossible}, name: __MODULE__)
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, %{clients: %{}, session_state: :impossible}, name: __MODULE__)
   end
-
 
   def init(state) do
     {:ok, state}
   end
 
-
-
-  # Public API
-  @spec on_session_started(any(), any()) :: any()
   def on_session_started(session_id, pid) do
-    Logger.info("on_session_started called for #{inspect(session_id)}")
-    GenServer.call(__MODULE__, {:start_session, session_id, pid})
+    GenServer.cast(__MODULE__, {:session_started, session_id, pid})
   end
 
   def on_message(session_id, message) do
-    GenServer.cast(__MODULE__, {:message, session_id, message})
+    GenServer.cast(__MODULE__, {:handle_message, session_id, message})
   end
 
   def on_session_close(session_id) do
-    GenServer.call(__MODULE__, {:close_session, session_id})
+    GenServer.cast(__MODULE__, {:session_close, session_id})
   end
 
-  def handle_call({:start_session, session_id, pid}, _from, %SessionManager{clients: clients} = state) do
-    Logger.info("handle_call for session: #{inspect(session_id)}")
-    Logger.info("handle_call for pid: #{inspect(pid)}")
+  # GenServer Callbacks
+  def handle_cast({:session_started, session_id, pid}, state) do
+    clients = Map.put(state.clients, session_id, pid)
+    session_state =
+      if map_size(clients) > 2 do
+        send(pid, {:close, :too_many_clients})
+        state.session_state
+      else
+        # new_state = if map_size(new_clients) >= 2, do: :ready, else: :impossible
+        if map_size(clients) == 2 do :ready else :impossible end
+        # :ready
+      end
 
-    if map_size(clients) >= 2 do
-        send(pid, :close)
-        {:reply, :too_many_clients, state}
-    else
-        Logger.info("Adding client #{inspect(session_id)}")
-        new_clients = Map.put(clients, session_id, pid)
-
-        new_state = if map_size(new_clients) >= 2, do: :ready, else: :impossible
-
-        # Notify about state update
-        notify_about_state_update(%{state | clients: new_clients, state: new_state})
-
-        {:reply, {:ok, session_id}, %{state | clients: new_clients}}
-    end
-end
-
-
-  def handle_call({:close_session, session_id}, _from, %SessionManager{clients: clients} = state) do
-    Logger.info("Closing session: #{inspect(session_id)}")
-    new_clients = Map.delete(clients, session_id)
-    new_state = if map_size(new_clients) < 2, do: :impossible, else: state.state
-    notify_about_state_update(%{state | clients: new_clients, state: new_state})
-    {:reply, :ok, %{state | clients: new_clients}}
+    notify_state_update(session_state, clients)
+    {:noreply, %{state | clients: clients, session_state: session_state}}
   end
 
-  def handle_cast({:message, session_id, message}, %SessionManager{clients: clients, state: state} = current_state) do
-    Logger.info("Received message from #{inspect(session_id)}: #{inspect(message)}")
-
-    cond do
-      String.starts_with?(message, "STATE") -> handle_state(session_id, clients[session_id], state)
-      String.starts_with?(message, "OFFER") -> handle_offer(session_id, message, current_state)
-      String.starts_with?(message, "ANSWER") -> handle_answer(session_id, message, current_state)
-      String.starts_with?(message, "ICE") -> handle_ice(session_id, message, current_state)
-    end
-
-    {:noreply, current_state}
-  end
-
-  # Internal Logic
-  defp handle_state(session_id, client_pid, state) do
-    Logger.info("Handling state request for #{inspect(session_id)}")
-    if Process.alive?(client_pid) do
-      send(client_pid, {:state, state})
-    else
-      Logger.error("Client #{inspect(session_id)} is not alive.")
+  def handle_cast({:handle_message, session_id, message}, state) do
+    case parse_message_type(message) do
+      :offer -> handle_offer(session_id, message, state)
+      :answer -> handle_answer(session_id, message, state)
+      :ice -> handle_ice(session_id, message, state)
+      _ -> {:noreply, state}
     end
   end
 
-  defp handle_offer(session_id, message, %SessionManager{clients: clients, state: :ready} = state) do
-    Logger.info("Handling offer from #{inspect(session_id)}")
-    target_client = find_target_client(session_id, clients)
-    if target_client do
-      send(target_client, {:offer, message})
-    else
-      Logger.warn("No target client available to send the offer.")
-    end
+  def handle_cast({:session_close, session_id}, state) do
+    clients = Map.delete(state.clients, session_id)
+    session_state = if map_size(clients) == 0, do: :impossible, else: state.session_state
+    notify_state_update(session_state, clients)
+    {:noreply, %{state | clients: clients, session_state: session_state}}
   end
 
-  defp handle_answer(session_id, message, %SessionManager{clients: clients, state: :creating} = state) do
-    Logger.info("Handling answer from #{inspect(session_id)}")
-    target_client = find_target_client(session_id, clients)
-    if target_client do
-      send(target_client, {:answer, message})
-    else
-      Logger.warn("No target client available to send the answer.")
-    end
-  end
-
-  defp handle_ice(session_id, message, %SessionManager{clients: clients} = state) do
-    Logger.info("Handling ICE candidate from #{inspect(session_id)}")
-    target_client = find_target_client(session_id, clients)
-    if target_client do
-      send(target_client, {:ice, message})
-    else
-      Logger.warn("No target client available to send the ICE candidate.")
-    end
-  end
-
-  defp find_target_client(session_id, clients) do
-    clients
-    |> Enum.filter(fn {id, _} -> id != session_id end)
-    |> Enum.map(&elem(&1, 1))
-    |> List.first()
-  end
-
-  defp notify_about_state_update(%SessionManager{clients: clients, state: state}) do
-    Logger.info("Clients: #{inspect(clients)}")
-    Logger.info("State: #{inspect(state)}")
-
-    Enum.each(clients, fn {key, pid} ->
-        if Process.alive?(pid) do
-            Logger.info("Before Sent state #{inspect(state)} to client #{inspect(key)}")
-          send(pid, {:state, state})
-          
-            Logger.info("AfterSent state #{inspect(state)} to client #{inspect(key)}")
-        else
-            Logger.error("Client #{inspect(key)} has a dead process with PID: #{inspect(pid)}")
-        end
+  defp notify_state_update(session_state, clients) do
+    Enum.each(clients, fn {_id, client_pid} ->
+      Logger.info("before send #{inspect(session_state)}  #{inspect(clients)}")
+      send(client_pid, {:state_update, session_state})
+      Logger.info("after send")
     end)
-end
+  end
+
+  # def handle_offer(session_id, message, %{clients: clients,session_state: :ready} = state) do
+  #   IO.puts "session_id "<>session_id<>" message "<>message
+  #   clients = Map.delete(state.clients, session_id)
+  #   IO.puts "clients #{inspect(clients)}"
+
+  #   [client_pid] = Map.values(clients)
+  #   IO.puts "client_pid ===== #{inspect(client_pid)}"
+  #   send(client_pid, {:offer, message})
+  #   {:noreply, %{state | session_state: :creating}}
+  # end
+
+  def handle_offer(session_id, message, %{clients: clients, session_state: :ready} = state) do
+    IO.puts "session_id "<>session_id<>" message "<>message
+    clients = Map.delete(state.clients, session_id)
+    IO.puts "clients #{inspect(clients)}"
+
+    [client_pid] = Map.values(clients)
+    IO.puts "client_pid ===== #{inspect(client_pid)}"
+    send(client_pid, {:offer, message})
+    {:noreply, %{state | session_state: :creating}}
+  end
+
+  # New clause to handle the :creating state
+  def handle_offer(session_id, message, %{clients: clients, session_state: :creating} = state) do
+    IO.puts "Session is still being created. Ignoring offer for session: #{session_id}"
+
+    IO.puts "session_id "<>session_id<>" message "<>message
+    clients = Map.delete(state.clients, session_id)
+    IO.puts "clients #{inspect(clients)}"
+
+    [client_pid] = Map.values(clients)
+    IO.puts "client_pid ===== #{inspect(client_pid)}"
+    send(client_pid, {:creating, message})
+    {:noreply, state}  # You can choose to log or take a different action
+  end
 
 
+  # defp handle_offer(session_id, message, %{session_state: :ready, clients: clients} = state) do
+  #   IO.puts "in handle offer===="
+  #   case Map.get(clients, session_id) do
+  #     nil ->
+  #       IO.puts "in handle offer nil "
+  #       # Handle the case where the session_id does not exist
+  #       {:stop, :session_not_found, state}
+
+  #     client_pid ->
+  #       IO.puts "in handle offer==== client_pid "<>client_pid
+  #       # Send the offer to the client process
+  #       send(client_pid, {:offer, message})
+  #       # Update the state to creating and remove the client
+  #       updated_clients = Map.delete(clients, session_id)
+  #       {:noreply, %{state | clients: updated_clients, session_state: :creating}}
+  #   end
+  # end
+
+  defp handle_answer(session_id, message, %{session_state: :creating} = state) do
+    clients = Map.delete(state.clients, session_id)
+    [client_pid] = Map.values(clients)
+    send(client_pid, {:answer, message})
+    {:noreply, %{state | session_state: :active}}
+  end
+
+  defp handle_ice(session_id, message, state) do
+    clients = Map.delete(state.clients, session_id)
+    [client_pid] = Map.values(clients)
+    send(client_pid, {:ice, message})
+    {:noreply, state}
+  end
+
+  defp parse_message_type(message) do
+    cond do
+      String.starts_with?(message, "OFFER") -> :offer
+      String.starts_with?(message, "ANSWER") -> :answer
+      String.starts_with?(message, "ICE") -> :ice
+      true -> :unknown
+    end
+  end
 end
--- lib/webrtc_phoenix_web/channels/room_channel.ex
+++ lib/webrtc_phoenix_web/channels/room_channel.ex
@@ -15,76 +15,189 @@ defmodule WebrtcPhoenixWeb.RoomChannel do
     # Task.start(fn -> SessionManager.on_session_started(session_id, self()) end)
     SessionManager.on_session_started(session_id, self())  # Notify SessionManager
 
-    send(self(), :after_join)  # Send message to self to handle incoming messages
+    # send(self(), :after_join)  # Send message to self to handle incoming messages
+    {:ok, %{session_id: session_id}, assign(socket, :session_id, session_id)}
 
-    {:ok, assign(socket, :session_id, session_id)}  # Assign session ID to the socket
+    # {:ok, assign(socket, :session_id, session_id)}  # Assign session ID to the socket
 
   end
 
-  @spec handle_in(<<_::56>>, map(), Phoenix.Socket.t()) :: {:noreply, Phoenix.Socket.t()}
-  def handle_in("new_msg", %{"body" => body}, socket) do
-    broadcast! socket, "new_msg", %{body: body}
+  # @spec handle_in(<<_::56>>, map(), Phoenix.Socket.t()) :: {:noreply, Phoenix.Socket.t()}
+  # def handle_in("new_msg", %{"body" => body}, socket) do
+  #   broadcast! socket, "new_msg", %{body: body}
+  #   {:noreply, socket}
+  # end
+
+  # def handle_in("start_session", _params, socket) do
+  #   Logger.info("Starting session for client #{socket.assigns.user_id}")
+  #   session_id = UUID.uuid4()
+  #   SessionManager.on_session_started(session_id, socket)
+  #   {:noreply, socket}
+  # end
+
+
+  # @spec handle_info(:after_join | {:state, any()}, atom() | map()) ::
+  #         {:noreply, Phoenix.Socket.t()}
+  #         | {:stop, any(), atom() | %{:assigns => atom() | map(), optional(any()) => any()}}
+  # def handle_info(:after_join, socket) do
+  #   # Loop to handle incoming messages
+  #   receive do
+  #     {:socket_frame, frame} ->
+  #       # Handle incoming frame (e.g., a message)
+  #       SessionManager.on_message(socket.assigns.session_id, frame)
+  #       # Continue to listen for more messages
+  #       handle_info(:after_join, socket)
+
+  #     {:exit, reason} ->
+  #       # Handle closure or exit reason
+  #       IO.puts("Exiting incoming loop, closing session: #{socket.assigns.session_id}")
+  #       SessionManager.on_session_close(socket.assigns.session_id)
+  #       {:stop, reason, socket}
+
+  #     # Catch-all for unhandled messages
+  #     _ ->
+  #       handle_info(:after_join, socket)
+  #   after
+  #     # Handle timeout or other cleanup logic if needed
+  #     60000 ->  # Adjust timeout as necessary
+  #       IO.puts("No messages received, closing session: #{socket.assigns.session_id}")
+  #       SessionManager.on_session_close(socket.assigns.session_id)
+  #       {:stop, :timeout, socket}
+  #   end
+  # end
+
+  #   # This is where we handle info messages
+  #   def handle_info({:state, :impossible}, socket) do
+  #     # Here you can decide what to do when the state is :impossible
+  #     # For instance, you might want to push a message to the client:
+  #     push(socket, "state_update", %{status: "impossible", message: "The session cannot be started."})
+
+  #     # Return the unchanged socket
+  #     {:noreply, socket}
+  #   end
+
+  #   # You may want to handle other states as well
+  #   def handle_info({:state, state}, socket) do
+  #     # Handle other states as needed
+  #     push(socket, "state_update", %{status: state})
+  #     {:noreply, socket}
+  #   end
+
+  # def terminate(_reason, socket) do
+  #   # Clean up when the channel is terminated
+  #   SessionManager.on_session_close(socket.assigns.session_id)
+  #   :ok
+  # end
+
+  @spec handle_in(
+          <<_::24, _::_*8>>,
+          map(),
+          atom() | %{:assigns => nil | maybe_improper_list() | map(), optional(any()) => any()}
+        ) ::
+          {:noreply,
+           atom() | %{:assigns => nil | maybe_improper_list() | map(), optional(any()) => any()}}
+  def handle_in("new_message", %{"message" => message}, socket) do
+    IO.puts "new message==="
+    session_id = socket.assigns[:session_id]
+    SessionManager.on_message(session_id, "OFFER " <> message)
     {:noreply, socket}
   end
 
-  def handle_in("start_session", _params, socket) do
-    Logger.info("Starting session for client #{socket.assigns.user_id}")
-    session_id = UUID.uuid4()
-    SessionManager.on_session_started(session_id, socket)
+  # def handle_in("offer", message, socket) when is_binary(message) do
+  #   session_id = socket.assigns[:session_id]
+
+  #   # Assuming the SDP offer is coming in as plain text
+  #   # Prepend "OFFER " to the message and pass it to the SessionManager
+  #   SessionManager.on_message(session_id, "OFFER " <> message)
+
+  #   {:noreply, socket}
+  # end
+
+
+  def handle_in("answer", %{"message" => message}, socket) do
+    IO.puts "answer"
+    session_id = socket.assigns[:session_id]
+    SessionManager.on_message(session_id, "ANSWER " <> message)
     {:noreply, socket}
   end
 
-
-  @spec handle_info(:after_join | {:state, any()}, atom() | map()) ::
-          {:noreply, Phoenix.Socket.t()}
-          | {:stop, any(), atom() | %{:assigns => atom() | map(), optional(any()) => any()}}
-  def handle_info(:after_join, socket) do
-    # Loop to handle incoming messages
-    receive do
-      {:socket_frame, frame} ->
-        # Handle incoming frame (e.g., a message)
-        SessionManager.on_message(socket.assigns.session_id, frame)
-        # Continue to listen for more messages
-        handle_info(:after_join, socket)
-
-      {:exit, reason} ->
-        # Handle closure or exit reason
-        IO.puts("Exiting incoming loop, closing session: #{socket.assigns.session_id}")
-        SessionManager.on_session_close(socket.assigns.session_id)
-        {:stop, reason, socket}
-
-      # Catch-all for unhandled messages
-      _ ->
-        handle_info(:after_join, socket)
-    after
-      # Handle timeout or other cleanup logic if needed
-      60000 ->  # Adjust timeout as necessary
-        IO.puts("No messages received, closing session: #{socket.assigns.session_id}")
-        SessionManager.on_session_close(socket.assigns.session_id)
-        {:stop, :timeout, socket}
-    end
+  def handle_in("ice", %{"message" => message}, socket) do
+    IO.puts "ice"
+    session_id = socket.assigns[:session_id]
+    SessionManager.on_message(session_id, "ICE " <> message)
+    {:noreply, socket}
   end
-
-    # This is where we handle info messages
-    def handle_info({:state, :impossible}, socket) do
-      # Here you can decide what to do when the state is :impossible
-      # For instance, you might want to push a message to the client:
-      push(socket, "state_update", %{status: "impossible", message: "The session cannot be started."})
-
-      # Return the unchanged socket
-      {:noreply, socket}
-    end
-
-    # You may want to handle other states as well
-    def handle_info({:state, state}, socket) do
-      # Handle other states as needed
-      push(socket, "state_update", %{status: state})
-      {:noreply, socket}
-    end
 
   def terminate(_reason, socket) do
-    # Clean up when the channel is terminated
-    SessionManager.on_session_close(socket.assigns.session_id)
+    session_id = socket.assigns[:session_id]
+    SessionManager.on_session_close(session_id)
     :ok
+  end
+
+  def handle_info({:state_update, :ready}, socket) do
+
+    push(socket, "state_update", %{state: "ready"})
+    {:noreply, socket}
+  end
+
+
+
+  def handle_info({:state_update, :impossible}, socket) do
+
+    push(socket, "state_update", %{state: "impossible"})
+    {:noreply, socket}
+  end
+
+  def handle_info({:state_update, :creating}, socket) do
+
+    push(socket, "state_update", %{state: "creating"})
+    {:noreply, socket}
+  end
+
+  def handle_info({:creating, offer}, socket) do
+    IO.inspect(offer, label: "Received WebRTC Offer")
+
+    # You can push the offer to the client or process it further
+    push(socket, "state_update", %{state: "creating", offer: offer})
+
+    {:noreply, socket}
+  end
+
+
+    #   # You may want to handle other states as well
+    # def handle_info({:state, state}, socket) do
+    #   # Handle other states as needed
+    #   push(socket, "state_update", %{status: state})
+    #   {:noreply, socket}
+    # end
+
+  def handle_info(msg, socket) do
+    IO.inspect(msg, label: "Unexpected message")
+    {:noreply, socket}
+  end
+
+   # Handle incoming WebSocket messages
+   def handle_in(message, socket) do
+    case Jason.decode(message) do
+      {:ok, json} ->
+        # Handle valid JSON messages
+        handle_json_message(json, socket)
+
+      {:error, _} ->
+        # Handle non-JSON messages (likely plain text like SDP offers)
+        handle_plain_text_message(message, socket)
+    end
+  end
+
+  defp handle_json_message(%{"type" => "offer", "sdp" => sdp}, socket) do
+    # Handle SDP offer here
+    push(socket, "sdp_offer", %{sdp: sdp})
+    {:noreply, socket}
+  end
+
+  defp handle_plain_text_message(sdp, socket) do
+    # Handle plain SDP offer here
+    push(socket, "sdp_offer", %{sdp: sdp})
+    {:noreply, socket}
   end
 end
